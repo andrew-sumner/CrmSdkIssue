12a76feb4c7db95cd248ce0e5db32e6bd5712e3c
Andrew Sumner <andrew.sumner@xtra.co.nz>
2019-11-05 18:04:57 +1300

    Initial Version

---
+++ CrmSdkIssue/Utils/AzureEnvironment.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmSdkIssue
+{
+    public static class AzureEnvironment
+    {
+        public static string GetSetting(string variable)
+        {
+            string setting = Environment.GetEnvironmentVariable(variable);
+
+            if (String.IsNullOrEmpty(setting))
+            {
+                throw new ArgumentException($"Setting '{variable}' is not configured.");
+            }
+
+            return setting;
+        }
+
+        public static bool GetSettingAsBoolean(string variable)
+        {
+            return Boolean.Parse(GetSetting(variable));
+        }
+
+        public static List<string> GetSettingAsList(string variable)
+        {
+            string list = GetSetting(variable);
+
+            return list.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(p => p.Trim()).ToList();
+        }
+
+        public static string GetSetting(string variable, string defaultValue)
+        {
+            string value = Environment.GetEnvironmentVariable(variable);
+
+            if (String.IsNullOrEmpty(value))
+            {
+                return defaultValue;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ CrmSdkIssue2/CrmSdkWorking/AzureKeyVault.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.Services.AppAuthentication;
+using System;
+using System.Threading.Tasks;
+
+namespace CrmSdkIssue
+{
+    /**
+     * Service-to-service authentication to Azure Key Vault 
+     * https://blogs.msdn.microsoft.com/cloud_solution_architect/2017/12/22/using-managed-service-identities-in-functions-to-access-key-vault/
+     * https://docs.microsoft.com/en-us/azure/key-vault/service-to-service-authentication
+     */
+    public static class AzureKeyVault
+    {
+        private static KeyVaultClient keyVaultClient = null;
+
+        static AzureKeyVault()
+        {
+            // NOTE: This only works when running via visual studio or logged on 
+            // Does not work from service even if assinged identity https://blog.bitscry.com/2019/02/13/using-azure-key-vault-in-a-console-application/
+
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+        }
+
+        /// <summary>
+        /// Get secret value from Azure Key Vault.
+        /// <param name="secret">Full secret url</param>
+        /// <returns>Secret value</returns>
+        public static string GetSecret(string secret)
+        {
+            return GetSecretAsync(secret).Result;
+        }
+
+        /// <summary>
+        /// Get secret value from Azure Key Vault.
+        /// <param name="secret">Full secret url</param>
+        /// <returns>Secret value</returns>
+        public static async Task<string> GetSecretAsync(string secretUrl)
+        {
+            SecretBundle result;
+
+            try
+            {
+                result = await keyVaultClient.GetSecretAsync(secretUrl).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                throw new KeyVaultErrorException($"Key Vault '{secretUrl}' secret was not found.", ex);
+            }
+
+            if (String.IsNullOrEmpty(result.Value))
+            {
+                throw new ArgumentException($"Key Vault '{secretUrl}' secret is not configured.");
+            }
+
+            return result.Value;
+        }
+    }
+}
